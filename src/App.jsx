c738e31b49cc757e4a4f1668cd0c2f07473e071e
aramebarseghyan4 <aramebarseghyan@gmail.com>
2026-08-06 22:52:25 +0400

    feat(scroll): add ScrollToTop component to reset scroll position on route change

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import MainHeader from "./components/MainHeader";
 import Footer from "./components/Footer/Footer";
 import PageHeader from "./components/PageHeader";
+import ScrollToTop from "./components/ScrollToTop"; // <-- Добавили новый импорт
 
 // 2. Импортируем страницы
 import HomePage from "./Pages/HomePage/HomePage";
@@ -13,11 +14,14 @@ import CreditHistoryPage from "./Pages/LoansPage/CreditHistoryPage";
 import LoansImportantInfo from "./Pages/LoansPage/LoansImportantInfo";
 import CardsPage from "./Pages/CardsPage/CardsPage";
 import CardDetailWindow from "./Pages/CardsPage/CardDetailWindow";
-import CardsProvisionAndService from "./Pages/CardsPage/CardsProvisionAndService"; // Импорт страницы условий карт
+import CardsProvisionAndService from "./components/ScrollToTop"; // Импорт страницы условий карт
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      {/* Этот компонент будет следить за URL и скроллить наверх */}
+      <ScrollToTop />
+
       <MainHeader />
 
       {/* Автоматически отображается на всех страницах, кроме Главной (HomePage) */}
